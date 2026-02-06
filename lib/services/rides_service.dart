d7ef9d7ee1457559f213876e76bb7994a1912f58
unknown <seavtheansithav@gmail.com>
2026-02-06 10:01:46 +0700

    BLA -001- Ride service

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,12 +1,41 @@
+import 'dart:math';
+
 import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> _filterByDeparture(Location departure) {
+    List<Ride> filterRide = [];
+    availableRides.map((e) {
+      if (departure == e.departureLocation) {
+        filterRide.add(e);
+      }
+    });
+    return filterRide;
+  }
 
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> _filterBySeatRequested(int requestedSeat) {
+    List<Ride> filterSeat = [];
+    availableRides.map((e) {
+      if (requestedSeat <= e.remainingSeats) {
+        filterSeat.add(e);
+      }
+    });
+    return filterSeat;
+  }
 
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return [];
+  }
 }
--- test/test_rides.dart
+++ test/test_rides.dart
@@ -1,12 +1,15 @@
-import '../lib/model/ride/ride.dart';
+import 'package:blabla/model/ride/ride.dart';
 
-import '../lib/model/ride/locations.dart';
-import '../lib/services/rides_service.dart';
+import 'package:blabla/model/ride/locations.dart';
+import 'package:blabla/services/rides_service.dart';
 
 void main() {
   Location dijon = Location(country: Country.france, name: "Dijon");
 
-  List<Ride> filteredRide = RidesService.filter(requestedSeats: 2, departureLocation: dijon);
+  List<Ride> filteredRide = RidesService.filter(
+    requestedSeats: 2,
+    departureLocation: dijon,
+  );
 
   for (Ride ride in filteredRide) {
     print(ride);
